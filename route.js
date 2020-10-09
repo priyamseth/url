686a10b64f21c8f680211cfd3ad9590c6823a535
Priyam <56730716+Signior-X@users.noreply.github.com>
2020-10-10 01:15:12 +0530

    Create route.js

---
+++ route.js
@@ -0,0 +1,15 @@
+var notFound = 0;
+var thisUrl = location.href;
+var code = thisUrl.split('/'); // 4th one is giving the answer or say the second last one
+var codef = code[code.length-2]
+console.log(codef)
+
+// Start here the if else ladder
+if (codef === 'pnotes'){ location.href = 'https://pnotes.web.app' }
+else if (codef === 'website') { location.href = 'https://signior-x.github.io' }
+else if (codef === 'song') { location.href = 'https://la-musica.herokuapp.com' }
+else if (codef === 'pwho') { location.href = 'https://pwho.herokuapp.com' }
+else if (codef === 'pol') { location.href='https://quiziit.ml/politicalquiz/' }
+else {
+    var notFound = 1;
+}
